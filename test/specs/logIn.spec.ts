7567bf50678a26edea08b33aec6862c44ac0c75c
aya-karim <livetolife42@gmail.com>
2022-03-25 23:09:35 +0200

    Refactore Spec file

--- test/specs/logIn.spec.ts
+++ test/specs/logIn.spec.ts
@@ -8,7 +8,7 @@ describe("Login from LogIn page", () => {
   });
 
   it("Login with Invalid credentials", async () => {
-    await LoginPage.SendLogInCredentials("test@test.com", "ThisIs@T3st");
+    await LoginPage.sendLogInCredentials("test@test.com", "ThisIs@T3st");
     await expect(LoginPage.errormsg).toBeExisting();
     await expect(LoginPage.errormsg).toHaveTextContaining(
       "Invalid login or password."
@@ -16,7 +16,7 @@ describe("Login from LogIn page", () => {
   });
 
   it("Login with empty credentials", async () => {
-    await LoginPage.SendLogInCredentials("", "");
+    await LoginPage.sendLogInCredentials("", "");
     await expect(LoginPage.missingEmailMsg).toHaveTextContaining(
       "This is a required field."
     );
